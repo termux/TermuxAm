1620e7c88bb1c120afa9301a423374b43637dcf1
Michal Bednarski <bednarski.michal2@gmail.com>
2019-08-07 19:15:36 +0200

    Add tests for IntentCmd parsing string array

---
+++ app/src/androidTest/java/com/example/termuxam/IntentCmdTest.java
@@ -0,0 +1,31 @@
+package com.example.termuxam;
+
+import android.content.Intent;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+public class IntentCmdTest {
+    @Test
+    public void testExtraStringArray() throws URISyntaxException {
+        ShellCommand shellCommand = new ShellCommand();
+        shellCommand.init(new String[] { "--esa", "extra-name", "aaa,bbb\\,ccc,ddd" }, 0);
+
+        Intent intent = IntentCmd.parseCommandArgs(shellCommand, null);
+        String[] resultExtra = intent.getStringArrayExtra("extra-name");
+        Assert.assertArrayEquals(new String[] { "aaa", "bbb,ccc", "ddd" }, resultExtra);
+    }
+
+    @Test
+    public void testExtraStringArrayList() throws URISyntaxException {
+        ShellCommand shellCommand = new ShellCommand();
+        shellCommand.init(new String[] { "--esal", "extra-name", "aaa,bbb\\,ccc,ddd" }, 0);
+
+        Intent intent = IntentCmd.parseCommandArgs(shellCommand, null);
+        ArrayList<String> resultExtra = intent.getStringArrayListExtra("extra-name");
+        Assert.assertArrayEquals(new Object[] { "aaa", "bbb,ccc", "ddd" }, resultExtra.toArray());
+    }
+}
